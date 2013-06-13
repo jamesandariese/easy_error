5e83127bb19b8245d0b85303f43999ec57e45acb
James Andariese <james.andariese@locationlabs.com>
2013-06-12 22:27:20 -0700

    initial import

---
+++ easy_error.go
@@ -0,0 +1,28 @@
+package easy_error
+
+import "errors"
+
+var ErrUnknown = errors.New("Unknown error")
+func Apply(r interface{}) (err error) {
+	if r != nil {
+		ee, ok := r.(error)
+		if !ok {
+			err = ErrUnknown
+		} else {
+			err = ee
+		}
+	}
+	return
+}
+
+func AssertNil(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Wrap(v interface{}, err error) (r interface{}) {
+	AssertNil(err)
+	return v
+}
+
